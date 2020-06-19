d7b59b430ad68d98b9686ff44bdcd017c4a1447b
Doris Castro <dorisgeo12@gmail.com>
2020-06-19 15:08:07 -0400

    heroku added

--- Develop/server.js
+++ Develop/server.js
@@ -14,11 +14,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(MONGODB_URI);
+
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api.js"));
