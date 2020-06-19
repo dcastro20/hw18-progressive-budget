924a8111aad58135651f4f676cfa54715f70e315
Doris Castro <dorisgeo12@gmail.com>
2020-06-18 21:22:51 -0400

    updated service worker

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,28 +1,18 @@
 const FILES_TO_CACHE = [
-  "/",
   "/index.html",
-  "/assets/css/style.css",
-  "/assets/js/loadPosts.js",
-  "/assets/images/Angular-icon.png",
-  "/assets/images/React-icon.png",
-  "/assets/images/Vue.js-icon.png",
-  "/manifest.webmanifest",
-  "/favicon.ico",
-  "/assets/images/icons/icon-72x72.png",
-  "/assets/images/icons/icon-96x96.png",
-  "/assets/images/icons/icon-128x128.png",
-  "/assets/images/icons/icon-144x144.png",
-  "/assets/images/icons/icon-152x152.png",
-  "/assets/images/icons/icon-192x192.png",
-  "/assets/images/icons/icon-384x384.png",
-  "/assets/images/icons/icon-512x512.png",
+  "/db.js",
+  "/index.js",
+  "/manifest.json",
+  "/style.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
-self.addEventListener("install", function(evt) {
+self.addEventListener("install", function (evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       console.log("Your files were pre-cached successfully!");
@@ -33,7 +23,7 @@ self.addEventListener("install", function(evt) {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", function(evt) {
+self.addEventListener("activate", function (evt) {
   evt.waitUntil(
     caches.keys().then(keyList => {
       return Promise.all(
@@ -51,7 +41,7 @@ self.addEventListener("activate", function(evt) {
 });
 
 // fetch
-self.addEventListener("fetch", function(evt) {
+self.addEventListener("fetch", function (evt) {
   // cache successful requests to the API
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
@@ -78,9 +68,8 @@ self.addEventListener("fetch", function(evt) {
   // if the request is not for the API, serve static assets using "offline-first" approach.
   // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
   evt.respondWith(
-    caches.match(evt.request).then(function(response) {
+    caches.match(evt.request).then(function (response) {
       return response || fetch(evt.request);
     })
   );
 });
-
